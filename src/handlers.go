c24349bd0487b9f90de7a99e64b256cb16e7ab1f
Gleicon Moraes <gleicon@gmail.com>
2013-08-02 17:45:59 -0300

    more handlers

--- src/handlers.go
+++ src/handlers.go
@@ -7,42 +7,52 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"regexp"
 )
 
 /* TaskCreateHandler creates new tasks. It accepts the following arguments:
 - cron:		Cron string to schedule the task
-- src[url]:	URL that's hit when cron executes
-- src[method]:	HTTP method used for accessing the source URL
-- src[body]:	Optional POST payload for the source URL
-- src[policy]:	Policy for accessing the source URL
+- src[url]:	URL that's hit when cron executes [default: empty - task GET dstUrl w/o any payload]
+- src[method]:	HTTP method used for accessing the source URL [default: GET]
+- src[body]:	Optional POST payload for the source URL [default: empty]
+- src[policy]:	Policy for accessing the source URL [default: once]
 - dst[url]:	URL that'll receive the task response
 - dst[policy]:	Policy for accessing the destination URL
 
 Supported policies are:
-- once:		Try once and give up if it fails
+- once:		Try once and give up if it fails [default]
 - persist:	Retry for ever until it works
 - retry,N:	Use N as the number of times to retry before giving up
 */
+
 func TaskCreateHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
 		http.Error(w, http.StatusText(405), 405)
 		return
 	}
-	t := &Task{
-		Cron: r.FormValue("cron"),
-		Src: TaskSrc{
+
+    crontabstring := r.FormValue("crontabstring")
+    dstURL := r.FormValue("dst[url]")
+
+    if crontabstring == "" || dstURL == "" {
+        http.Error(w, "Bad request: empty crontabstring or dst[url]", 400)
+		return
+    }
+    // TODO: check formvalues for nil or empty
+
+    tS := TaskSrc {
 			URL:    r.FormValue("src[url]"),
 			Method: r.FormValue("src[method]"),
 			Body:   r.FormValue("src[body]"),
 			Policy: r.FormValue("src[policy]"),
-		},
-		Dst: TaskDst{
-			URL:    r.FormValue("dst[url]"),
+    }
+
+    tD := TaskDst{
+			URL:    dstURL,
 			Policy: r.FormValue("dst[policy]"),
-		},
-	}
+    }
+
+    id, t := NewTask(crontabstring, tS, tD)
 	fmt.Println(t)
 }
 
